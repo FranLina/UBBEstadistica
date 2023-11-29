75dff29476a9b583c23fd67c174457039d279b4d
Francisco Linares Barragán <francisco.linares.536@estudiante.iesoretania.es>
2023-11-29 13:16:37 +0100

    commit inicial

---
+++ app/src/main/java/com/franciscolinares/ubb/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.franciscolinares.ubb
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.franciscolinares.ubb.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
